533ffd62b7fe293bc240add7725c4820b357791a
emin-skrijelj <86375108+emin-skrijelj@users.noreply.github.com>
2024-05-31 01:20:31 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://emin-skrijelj.github.io",
-  base: "/my-blog",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
