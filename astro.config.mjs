428877235b13b8ecb35ef4fada6257fbadfba2d3
emin-skrijelj <86375108+emin-skrijelj@users.noreply.github.com>
2024-05-31 01:01:38 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,8 +30,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://emin-skrijelj.github.io",
+  base: "/my-blog",
   trailingSlash: "always",
   integrations: [
     tailwind(),
